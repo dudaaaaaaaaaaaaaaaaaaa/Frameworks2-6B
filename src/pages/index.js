a6411fa54560deb1bf879d2989de5678736182c8
dudaaaaaaaaaaaaaaaaaaa <119938999+dudaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2024-09-27 14:22:14 +0000

    aula 27-09.2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
+import Footer from "./component/footer";
+import Navbar from "./component/navbar";
+
 function Index() {
-  return <h1>Olá mundo, eu estou vivo</h1>
-  
+
+  return
+  <Navbar />
+  <> <h1>Olá mundo, eu estou vivo</h1></>
+  <Footer />
+ 
 }
 export default Index;
